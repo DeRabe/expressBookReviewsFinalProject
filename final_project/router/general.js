da7ba132910e3df446c2829e014e03f778e174fb
Denise <denise@soerensen.com>
2025-03-19 13:01:03 +0100

    Implement book listing and detail retrieval endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,17 +11,29 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  // Send the list of all books in a neat JSON format
+  res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+  // Retrieve the ISBN parameter from the request
+  const isbn = req.params.isbn;
+
+  // Find the book in our 'books' object using the ISBN as key
+  const book = books[isbn];
+
+  // Check if the book exists
+  if (book) {
+    // Return book details in neat JSON format
+    res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+    // If no book found with the given ISBN, return a 404 error
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
@@ -41,3 +53,7 @@ public_users.get('/review/:isbn',function (req, res) {
 });
 
 module.exports.general = public_users;
+
+// curl localhost:5000
+
+// curl localhost:5000/isbn/1
